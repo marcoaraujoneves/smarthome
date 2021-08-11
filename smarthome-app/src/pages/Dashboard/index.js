a53414ac3c60389256a246de960e8ce3dc20ded0
Marco Araujo <marcoaraujoneves@gmail.com>
2021-08-11 18:32:59 -0300

    Adds empty-dashboard screen

--- smarthome-app/src/pages/Dashboard/index.js
+++ smarthome-app/src/pages/Dashboard/index.js
@@ -1,29 +1,56 @@
 import React, {useState, useEffect} from 'react';
-import {SafeAreaView, StyleSheet, Text} from 'react-native';
+import {SafeAreaView, StyleSheet, Text, TouchableOpacity} from 'react-native';
 import auth from '@react-native-firebase/auth';
 import database from '@react-native-firebase/database';
 
 export default function Dashboard({navigation}) {
   const [user, setUser] = useState({});
+  const [home, setHome] = useState({});
 
   useEffect(() => {
     const loadUserData = async () => {
       const {uid: userId} = auth().currentUser;
 
-      const userData = await database().ref(`/user/${userId}`).once('value');
+      const userSnap = await database().ref(`/user/${userId}`).once('value');
+      const userData = userSnap.val();
+
       setUser(userData);
 
       if (!userData.home) {
         navigation.navigate('Register Home');
+        return;
       }
+
+      const homeSnap = await database()
+        .ref(`home/${userData.home}`)
+        .once('value');
+
+      if (!homeSnap.exists()) {
+        navigation.navigate('Register Home');
+        return;
+      }
+
+      const homeData = homeSnap.val();
+      setHome(homeData);
     };
 
     loadUserData();
   }, [navigation]);
 
-  return (
+  return home && home.components ? (
     <SafeAreaView style={styles.container}>
       <Text>Dashboard {user ? user.name : ''}</Text>
+    </SafeAreaView>
+  ) : (
+    <SafeAreaView style={styles.container}>
+      <TouchableOpacity style={styles.addButton}>
+        <Text style={styles.addButtonText}>+</Text>
+      </TouchableOpacity>
+
+      <Text style={styles.emptyScreenMessage}>
+        To start using this app, you need to add rooms and components. Just
+        click the button above!
+      </Text>
     </SafeAreaView>
   );
 }
@@ -35,5 +62,31 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     backgroundColor: '#121212',
     alignSelf: 'stretch',
+  },
+
+  addButton: {
+    width: 205,
+    height: 205,
+    borderColor: '#486581',
+    borderStyle: 'dashed',
+    borderWidth: 4,
+    borderRadius: 40,
+    marginBottom: 48,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+
+  addButtonText: {
+    fontSize: 200,
+    lineHeight: 210,
+    color: '#486581',
+  },
+
+  emptyScreenMessage: {
+    color: '#ffffff',
+    fontSize: 16,
+    fontFamily: 'Roboto',
+    lineHeight: 20,
+    textAlign: 'center',
   },
 });
