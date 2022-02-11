2eda63dde645175faa7bbd4e68daa5cdcd93fec5
Marco Araujo <marcoaraujoneves@gmail.com>
2022-02-11 18:59:38 -0300

    New version of component connection handler using state machine

--- smarthome-esp32/sensors/temperatureSensor/temperatureSensor.ino
+++ smarthome-esp32/sensors/temperatureSensor/temperatureSensor.ino
@@ -1,66 +1,33 @@
-#include <Arduino.h>
-#include <WiFi.h>
-#include <Preferences.h>
+#include "wifiConnector.h"
+#include "secrets.h"
 // Firebase dependencies
 #include <Firebase_ESP_Client.h>
 #include "addons/TokenHelper.h"
 #include "addons/RTDBHelper.h"
-#include "secrets.h"
 
 // Sensor constants
 #define adcVoltageReference 5000.0
 #define adcResolution 4096.0
 #define sensorPin A0
 
-Preferences preferences;
-
 FirebaseData firebaseDataObject;
 FirebaseAuth auth;
 FirebaseConfig config;
 bool signupOK = false;
 
-String componentId = "";
-
-bool connectToWiFi()
+enum STATES
 {
-  preferences.begin("credentials", true);
+  CONNECTING,
+  CONNECTED,
+  SUCCESS,
+  FAILED,
+  SCANNING,
+  WAITING
+};
 
-  String WIFI_SSID = preferences.getString("WIFI_SSID", "");
-  String WIFI_PASSWORD = preferences.getString("WIFI_PASSWORD", "");
+enum STATES currentState;
 
-  preferences.end();
-
-  if (WIFI_SSID == "" || WIFI_PASSWORD == "")
-  {
-    return false;
-  }
-  else
-  {
-    WiFi.begin(WIFI_SSID.c_str(), WIFI_PASSWORD.c_str());
-    Serial.println("Connecting to Wi-Fi ...");
-
-    while (WiFi.status() != WL_CONNECTED)
-    {
-      delay(300);
-    }
-
-    Serial.println("Connected");
-    return true;
-  }
-}
-
-void savePasswordCredentials(String ssid, String password)
-{
-  preferences.begin("credentials", false);
-
-  preferences.putString("WIFI_SSID", ssid);
-  preferences.putString("WIFI_PASSWORD", password);
-
-  preferences.end();
-
-  delay(1000);
-  connectToWiFi();
-}
+bool connectionSuccess;
 
 float getTemperature()
 {
@@ -71,41 +38,34 @@ float getTemperature()
   return temperatureInCelcius;
 }
 
-void setup()
+void signupToFirebase()
 {
-  Serial.begin(115200);
+  config.api_key = API_KEY;
+  config.database_url = DATABASE_URL;
 
-  bool successfullyConnected = connectToWiFi();
-
-  if (successfullyConnected)
+  // Anonymous sign-in
+  if (Firebase.signUp(&config, &auth, "", ""))
   {
-    config.api_key = API_KEY;
-    config.database_url = DATABASE_URL;
-
-    // Anonymous sign-in
-    if (Firebase.signUp(&config, &auth, "", ""))
-    {
-      signupOK = true;
-    }
-    else
-    {
-      Serial.printf("%s\n", config.signer.signupError.message.c_str());
-    }
-
-    config.token_status_callback = tokenStatusCallback;
-
-    Firebase.begin(&config, &auth);
-    Firebase.reconnectWiFi(true);
+    signupOK = true;
   }
+  else
+  {
+    Serial.printf("%s\n", config.signer.signupError.message.c_str());
+  }
+
+  config.token_status_callback = tokenStatusCallback;
+
+  Firebase.begin(&config, &auth);
+  Firebase.reconnectWiFi(true);
 }
 
-void loop()
+void processComponentWork()
 {
   if (Firebase.ready() && signupOK)
   {
     float temperature = getTemperature();
 
-    if (Firebase.RTDB.setFloat(&firebaseDataObject, "component/" + componentId + "/reads", temperature))
+    if (Firebase.RTDB.setFloat(&firebaseDataObject, "component/" + COMPONENT_ID + "/reads", temperature))
     {
       Serial.println("PASSED");
       Serial.println("PATH: " + firebaseDataObject.dataPath());
@@ -117,6 +77,97 @@ void loop()
       Serial.println("REASON: " + firebaseDataObject.errorReason());
     }
   }
+  else
+  {
+    signupToFirebase();
+  }
+}
 
-  delay(10000);
+void setup()
+{
+  Serial.begin(115200);
+
+  getStoredCredentials();
+
+  if (ssid != "" && password != "")
+  {
+    currentState = CONNECTING;
+  }
+  else
+  {
+    currentState = SCANNING;
+  }
+}
+
+void loop()
+{
+  switch (currentState)
+  {
+  case CONNECTED:
+    Serial.println("CONNECTED");
+
+    processComponentWork();
+
+    delay(10000);
+    break;
+
+  case SCANNING:
+    Serial.println("SCANNING");
+
+    getAvailableNetworks();
+
+    currentState = WAITING;
+    break;
+
+  case WAITING:
+    Serial.println("WAITING");
+
+    if (!isBLEInitialized)
+    {
+      initBLE();
+      delay(2000);
+    }
+
+    if (ssid != "" && password != "")
+    {
+      currentState = CONNECTING;
+    }
+
+    delay(5000);
+    break;
+
+  case CONNECTING:
+    Serial.println("CONNECTING");
+
+    connectionSuccess = connectToWiFi(ssid, password);
+
+    currentState = connectionSuccess ? SUCCESS : FAILED;
+    break;
+
+  case FAILED:
+    Serial.println("FAILED");
+
+    ssid = "";
+    password = "";
+    connectionError = true;
+    currentState = SCANNING;
+    break;
+
+  case SUCCESS:
+    Serial.println("SUCCESS");
+
+    saveCredentials();
+
+    if (isBLEInitialized)
+    {
+      finishBLE();
+    }
+
+    connectionError = false;
+    currentState = CONNECTED;
+    break;
+
+  default:
+    break;
+  }
 }
---
+++ smarthome-esp32/sensors/temperatureSensor/wifiConnector.h
@@ -0,0 +1,180 @@
+#include <Arduino.h>
+#include <ArduinoJson.h>
+#include <Preferences.h>
+#include <WiFi.h>
+
+#include <BLEUtils.h>
+#include <BLEServer.h>
+#include <BLEDevice.h>
+#include <BLEAdvertising.h>
+
+#define SERVICE_UUID "5617df76-39f3-44b0-aa1b-c8e71e4caeba"
+#define CHARACTERISTIC_UUID "564c9b11-b549-4af0-9675-75225dba6db2"
+String COMPONENT_ID = "91cef537-feff-45cf-aeed-d4acddd33540";
+
+String availableNetworks[20] = {""};
+String ssid = "";
+String password = "";
+bool connectionError = false;
+
+Preferences preferences;
+StaticJsonBuffer<200> jsonBuffer;
+
+BLECharacteristic *pCharacteristic;
+BLEAdvertising *pAdvertising;
+BLEService *pService;
+BLEServer *pServer;
+bool isBLEInitialized = false;
+
+class CustomCharacteristicCallbacks : public BLECharacteristicCallbacks
+{
+  void onWrite(BLECharacteristic *pCharacteristic)
+  {
+    std::string value = pCharacteristic->getValue();
+
+    if (value.length() == 0)
+    {
+      return;
+    }
+
+    Serial.println("Received over BLE: " + String((char *)&value[0]));
+    JsonObject &jsonIn = jsonBuffer.parseObject((char *)&value[0]);
+
+    if (jsonIn.success())
+    {
+      if (jsonIn.containsKey("ssid") &&
+          jsonIn.containsKey("password"))
+      {
+        ssid = jsonIn["ssid"].as<String>();
+        password = jsonIn["password"].as<String>();
+
+        Serial.println("Received over bluetooth:");
+        Serial.println("SSID: " + ssid + ", password: " + password);
+      }
+    }
+    else
+    {
+      Serial.println("Received invalid JSON");
+    }
+
+    jsonBuffer.clear();
+  };
+
+  void onRead(BLECharacteristic *pCharacteristic)
+  {
+    Serial.println("BLE onRead request");
+    String response;
+
+    JsonObject &jsonOut = jsonBuffer.createObject();
+    JsonArray &ssids = jsonOut.createNestedArray("ssids");
+
+    jsonOut["key"] = COMPONENT_ID;
+    jsonOut["error"] = connectionError;
+
+    for (int i = 0; i < 20; i++)
+    {
+      if (availableNetworks[i] != "")
+      {
+        ssids.add(availableNetworks[i]);
+      }
+    }
+
+    jsonOut.printTo(response);
+
+    Serial.println(response);
+
+    pCharacteristic->setValue(
+        (uint8_t *)&response[0], response.length());
+
+    jsonBuffer.clear();
+  }
+};
+
+void finishBLE()
+{
+  pAdvertising->stop();
+
+  isBLEInitialized = false;
+}
+
+void initBLE()
+{
+  BLEDevice::init("SmartHome: Temperature");
+  BLEDevice::setPower(ESP_PWR_LVL_P7);
+
+  pServer = BLEDevice::createServer();
+
+  // pServer->setCallbacks(new CustomServerCallbacks());
+
+  pService = pServer->createService(BLEUUID(SERVICE_UUID), 20);
+
+  pCharacteristic = pService->createCharacteristic(
+      BLEUUID(CHARACTERISTIC_UUID),
+      BLECharacteristic::PROPERTY_READ |
+          BLECharacteristic::PROPERTY_WRITE);
+  pCharacteristic->setCallbacks(new CustomCharacteristicCallbacks());
+
+  pService->start();
+
+  pAdvertising = pServer->getAdvertising();
+  pAdvertising->start();
+
+  isBLEInitialized = true;
+}
+
+void getStoredCredentials()
+{
+  preferences.begin("credentials", true);
+
+  String WIFI_SSID = preferences.getString("WIFI_SSID", "");
+  String WIFI_PASSWORD = preferences.getString("WIFI_PASSWORD", "");
+
+  preferences.end();
+
+  if (WIFI_SSID != "" && WIFI_PASSWORD != "")
+  {
+    ssid = WIFI_SSID;
+    password = WIFI_PASSWORD;
+  }
+}
+
+void saveCredentials()
+{
+  preferences.begin("credentials", false);
+
+  preferences.putString("WIFI_SSID", ssid);
+  preferences.putString("WIFI_PASSWORD", password);
+
+  preferences.end();
+}
+
+void getAvailableNetworks()
+{
+  WiFi.mode(WIFI_STA);
+
+  int numberOfNetworks = WiFi.scanNetworks();
+
+  if (numberOfNetworks > 0)
+  {
+    for (int i = 0; i < numberOfNetworks; ++i)
+    {
+      availableNetworks[i] = WiFi.SSID(i);
+    }
+  }
+}
+
+bool connectToWiFi(String selectedSSID, String selectedPassword)
+{
+  WiFi.begin(selectedSSID.c_str(), selectedPassword.c_str());
+  Serial.println("Connecting to Wi-Fi ...");
+
+  // TODO: Failed connection is not handled properly
+  int tries = 0;
+  while (WiFi.status() != WL_CONNECTED || tries <= 5)
+  {
+    tries++;
+    delay(300);
+  }
+
+  return WiFi.status() == WL_CONNECTED;
+}
